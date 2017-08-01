fbb1fccfcf01f467e7716d9d79d19f4d6eaa89a9
zhangxiaogang <zhangxiaogang@xiaomi.com>
2017-08-01 22:26:34 +0800

    tried other parameters

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.134611, 0.000270736, 3.05349);
+  // pid.Init(0.134611, 0.000270736, 3.05349);  // tuned by others 
+  // pid.Init(0.2, 0.004, 3.0);  // failed
+  pid.Init(0.2, 0.0004, 3.0); // worked. 
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
